28fd5f1e123f4c86451b00341dd32933c4331386
Yoosun-Jang <ceusun0815@naver.com>
2023-08-12 18:01:01 +0900

    유선

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./page/loginPage";
 import MainPage from "./page/mainPage";
 import DetailPage from "./page/detailPage";
@@ -28,17 +28,6 @@ const StyledApp = styled.div`
   min-height: 100vh;
 `;
 
-function PrivateRoute({ children, ...rest }) {
-  const isLoggedIn = checkLoginStatus(); // 쿠키에서 로그인 상태 확인
-
-  return (
-    <Route
-      {...rest}
-      element={isLoggedIn ? children : <Navigate to="/login" />}
-    />
-  );
-}
-
 function App() {
   return (
     <>
@@ -50,7 +39,7 @@ function App() {
             <Route path="/main" element={<MainPage />} />
             <Route path="/detail/:StockID1" element={<DetailPage />} />
             <Route path="/search" element={<SearchPage />} />
-            <PrivateRoute path="/buy/:StockID2" element={<BuyPage />} />
+            <Route path="/buy/:StockID2" element={<BuyPage />} />
             <Route path="/buy/confirm" element={<BuyConfirmPage />} />
             <Route path="/sell/:StockID3" element={<SellPage />} />
             <Route path="/sell/confirm" element={<SellConfirmPage />} />
@@ -65,21 +54,5 @@ function App() {
     </>
   );
 }
-
-// 실제 쿠키에서 로그인 상태 정보를 확인하는 함수
-function checkLoginStatus() {
-  // 쿠키에서 'isLoggedIn' 값을 찾아 로그인 상태를 확인하는 로직
-  const cookies = document.cookie.split("; ");
-  const isLoggedInCookie = cookies.find((cookie) => cookie.startsWith("isLoggedIn="));
-
-  if (isLoggedInCookie) {
-    const isLoggedInValue = isLoggedInCookie.split("=")[1];
-    return isLoggedInValue === "true"; // 예시: "true"면 로그인 상태
-  }
-
-  return false; // 쿠키에 isLoggedIn이 없으면 로그아웃 상태
-}
-
-
 
 export default App;

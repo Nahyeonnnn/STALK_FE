9e79ce94651018d68a2beb68ca2293762a24cf0f
Yoosun-Jang <ceusun0815@naver.com>
2023-08-13 16:58:53 +0900

    로고

--- src/page/searchPage.jsx
+++ src/page/searchPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchBar from "../component/search/searchBar";
 import BottomBar from "../component/global/bottomBar";
+import Stalklogo from "../component/search/StalkLogo";
 const SearchPage = () => {
   return (
     <>
       <SearchBar></SearchBar>
+      <Stalklogo></Stalklogo>
       <BottomBar></BottomBar>
     </>
   );

f7be693589816931dd7fafead26f75df59455280
sunjae <zxsunje775@naver.com>
2023-08-14 15:03:07 +0900

    detail종목 좋아요애니메이션

--- src/component/detail/likebtn/likebtn.jsx
+++ src/component/detail/likebtn/likebtn.jsx
@@ -1,31 +1,58 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const LikeBtnBox = styled.div`
   position: absolute;
   display: flex;
-  top: 10vh;
-  left: 8vw;
-  cursor: pointer; /* 포인터 커서 표시 */
+  top: 11vh;
+  right: 15vw;
+  cursor: pointer;
+  z-index: 500;
 `;
 
 const HeartIcon = styled.span`
   /* font-size: 15px; */
-  color: ${(props) =>
-    props.liked ? "red" : "black"}; /* 클릭 여부에 따라 색상 변경 */
+  color: ${(props) => (props.liked ? "red" : "black")};
+  transition: font-size 0.3s;
+`;
+
+const heartbeatAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+const AnimatedHeartIcon = styled(HeartIcon)`
+  animation: ${(props) => (props.animate ? heartbeatAnimation : "none")} 0.5s
+    ease-in-out;
 `;
 
 const Likebtn = () => {
   const [liked, setLiked] = useState(false);
+  const [animate, setAnimate] = useState(false);
 
   const toggleLike = () => {
     setLiked(!liked);
+    setAnimate(true);
+
+    // 애니메이션 재생이 끝나면 다시 false로 설정하여 준비상태로 만듦
+    setTimeout(() => {
+      setAnimate(false);
+    }, 500);
   };
 
   return (
     <>
       <LikeBtnBox onClick={toggleLike}>
-        <HeartIcon liked={liked}>{liked ? "❤" : "♡"}</HeartIcon>
+        <AnimatedHeartIcon liked={liked} animate={animate}>
+          {liked ? "❤" : "♡"}
+        </AnimatedHeartIcon>
       </LikeBtnBox>
     </>
   );

79f147d93e1370418dbdeeb9e9fb609493191320
nahyeon <k92544199@gmail.com>
2023-08-15 02:22:38 +0900

    해외주식 디테일 그래프 모두 음향화 완료

--- src/component/detail/internationalGraphlist/year.jsx
+++ src/component/detail/internationalGraphlist/year.jsx
@@ -29,6 +29,7 @@ const Year = (props) => {
         },
       })
       .then((res) => {
+        setLista(res.data.lista);
         setStockData(res.data.data);
 
         setMaxPrice(
@@ -142,10 +143,46 @@ const Year = (props) => {
     },
   };
 
+  useEffect(() => {
+    //lista 저장 후 데이터를 소리로 변환
+    if (lista !== null) {
+      axios
+        .post(
+          `https://stalksound.store/sonification/data_to_sound/`,
+          {
+            lista: lista,
+          },
+          { responseType: "arraybuffer" }
+        ) //arraybuffer 형태로 받아서
+        .then(async (res) => {
+          console.log(res); //AudioContext 생성
+          const audioContext = new (window.AudioContext ||
+            window.webkitAudioContext)();
+          const decodedBuffer = await audioContext.decodeAudioData(res.data); //decode
+          setAudioBuffer(decodedBuffer); //디코딩된 정보 저장
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  }, [lista]);
+
+  //그래프 음향 출력
+  const playAudio = () => {
+    if (audioBuffer) {
+      const audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)();
+      const source = audioContext.createBufferSource();
+      source.buffer = audioBuffer;
+      source.connect(audioContext.destination);
+      source.start(0);
+    }
+  };
+
   return (
-    <>
+    <div onClick={playAudio}>
       <HighchartsReact highcharts={Highcharts} options={options} />
-    </>
+    </div>
   );
 };
 

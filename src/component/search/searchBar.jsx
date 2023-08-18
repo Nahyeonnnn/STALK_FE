3b995b11caed6a971886f04853c405e5298cefb6
kim nahyeon <stome6322@inha.edu>
2023-08-18 16:26:33 +0900

    서치바 배포 오류 해결

--- src/component/search/searchBar.jsx
+++ src/component/search/searchBar.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import NaverIcon from "./NaverIcon.png";
+// import NaverIcon from "./NaverIcon.png";
 import AudioRecord from "./audioRecord";
 
 library.add(faMagnifyingGlass);
@@ -702,7 +702,7 @@ const SearchBar = () => {
         console.log(e);
       });
       
-  }, []);
+  }, [recentAxios]);
 
   // useEffect(() => {
   //   async function fetchData() {

554a614dee58d1d4aaa57d1081e14857665e8d3e
nahyeon <k92544199@gmail.com>
2023-08-18 22:38:50 +0900

    둘러보기 버튼 추가

--- src/page/loginPage.jsx
+++ src/page/loginPage.jsx
@@ -2,17 +2,32 @@ import React from "react";
 import KakaoLoginBox from "../component/login/kakaoLoginBox";
 import StalkLogo from "../component/login/StalkLogo";
 import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const LoginDiv = styled.div`
   display: block;
   text-align: center;
 `;
 
+const MainBtn = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 5rem;
+  color: #f1d00a;
+  text-decoration: underline;
+  font-size: 1rem;
+`;
+
 const LoginPage = () => {
+  const navigate = useNavigate();
+  function moveToMain(){
+    navigate(`/main`);
+  }
   return (
     <LoginDiv>
       <StalkLogo></StalkLogo>
       <KakaoLoginBox></KakaoLoginBox>
+      <MainBtn onClick={moveToMain}>둘러보기</MainBtn>
     </LoginDiv>
   );
 };
